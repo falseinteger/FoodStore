ea85a67e78d7cb120ef6cbd90d7728ec0ad228ef
Dmitry Poroshkov <falseinteger@gmail.com>
2019-05-29 15:31:05 +0300

    Initial Commit

---
+++ FoodStore/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FoodStore
+//
+//  Created by Дмитрий Порошков on 29/05/2019.
+//  Copyright © 2019 Дмитрий Порошков. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
